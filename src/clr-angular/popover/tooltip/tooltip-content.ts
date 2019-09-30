dedc252b9f4df466b4d5fbc49cb592cc5b4bee08
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2019-09-30 13:21:30 +0300

    fix: just save to test github actions
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- src/clr-angular/popover/tooltip/tooltip-content.ts
+++ src/clr-angular/popover/tooltip/tooltip-content.ts
@@ -5,35 +5,115 @@
  */
 import { Component, ElementRef, Inject, Injector, Input, Optional } from '@angular/core';
 import { AbstractPopover } from '../common/abstract-popover';
-import { Point } from '../common/popover';
 import { POPOVER_HOST_ANCHOR } from '../common/popover-host-anchor.token';
 import { UNIQUE_ID } from '../../utils/id-generator/id-generator.service';
 import { TooltipIdService } from './providers/tooltip-id.service';
+
+import { ClrPopoverToggleService } from '../../utils/popover/providers/popover-toggle.service';
+import { ClrPopoverPosition } from '../../utils/popover/interfaces/popover-position.interface';
+import { ClrAlignment } from '../../utils/popover/enums/alignment.enum';
+import { ClrSide } from '../../utils/popover/enums/side.enum';
+import { ClrAxis } from '../../utils/popover/enums/axis.enum';
+import { Subscription } from 'rxjs';
 
 const POSITIONS: string[] = ['bottom-left', 'bottom-right', 'top-left', 'top-right', 'right', 'left'];
 
 const SIZES: string[] = ['xs', 'sm', 'md', 'lg'];
 
+let clrTooltipIds = 0;
+
+// @TODO rework this later on
+const SIMPLE_TO_SMART_POSITIONS: { [input: string]: ClrPopoverPosition } = {
+  'top-left': {
+    axis: ClrAxis.VERTICAL,
+    side: ClrSide.BEFORE,
+    anchor: ClrAlignment.CENTER,
+    content: ClrAlignment.END,
+  },
+  'top-right': {
+    axis: ClrAxis.VERTICAL,
+    side: ClrSide.BEFORE,
+    anchor: ClrAlignment.CENTER,
+    content: ClrAlignment.START,
+  },
+  right: {
+    axis: ClrAxis.HORIZONTAL,
+    side: ClrSide.AFTER,
+    anchor: ClrAlignment.CENTER,
+    content: ClrAlignment.CENTER,
+  },
+  'bottom-right': {
+    axis: ClrAxis.HORIZONTAL,
+    side: ClrSide.AFTER,
+    anchor: ClrAlignment.CENTER,
+    content: ClrAlignment.START,
+  },
+  'bottom-left': {
+    axis: ClrAxis.VERTICAL,
+    side: ClrSide.AFTER,
+    anchor: ClrAlignment.CENTER,
+    content: ClrAlignment.END,
+  },
+  left: {
+    axis: ClrAxis.HORIZONTAL,
+    side: ClrSide.BEFORE,
+    anchor: ClrAlignment.CENTER,
+    content: ClrAlignment.CENTER,
+  },
+  default: {
+    axis: ClrAxis.VERTICAL,
+    side: ClrSide.AFTER,
+    anchor: ClrAlignment.CENTER,
+    content: ClrAlignment.START,
+  },
+};
+
 @Component({
   selector: 'clr-tooltip-content',
   template: `
+    <div class="tooltip tooltip-extend"
+      [id]="popoverId"
+      [attr.aria-hidden]="!open"
+      [attr.id]="popoverId"
+      *clrPopoverContent="open at smartPosition; outsideClickToClose: true; scrollToClose: true">
+      <div class="tooltip-content tooltip-content-extend" [ngClass]="[positionClass, sizeClass]">
         <ng-content></ng-content>
+      </div>
+    </div>
     `,
+  styles: [
+    '.tooltip-extend { position: absolute !important }',
+    '.tooltip-content-extend { visibility: visible; opacity: 1 }',
+  ],
   host: {
-    '[class.tooltip-content]': 'true',
-    '[style.opacity]': '1',
+    // @TODO remove commented code
+    //'[class.tooltip-content]': 'true',
+    //'[style.opacity]': '1',
     '[attr.role]': '"tooltip"',
-    '[id]': 'id',
+    '[id]': 'popoverId',
   },
 })
 export class ClrTooltipContent extends AbstractPopover {
+  private subscriptions: Subscription[] = [];
+
+  public smartPosition: ClrPopoverPosition = {
+    axis: ClrAxis.HORIZONTAL,
+    side: ClrSide.AFTER,
+    anchor: ClrAlignment.CENTER,
+    content: ClrAlignment.CENTER,
+  };
+
   constructor(
     injector: Injector,
     @Optional()
     @Inject(POPOVER_HOST_ANCHOR)
     parentHost: ElementRef,
     @Inject(UNIQUE_ID) public uniqueId: string,
-    private tooltipIdService: TooltipIdService
+    // @TODO do I need this service ?
+    private tooltipIdService: TooltipIdService,
+    // Smart PopOver
+    private smartToggleService: ClrPopoverToggleService,
+    @Inject(UNIQUE_ID) public popoverId: string
   ) {
     super(injector, parentHost);
 
@@ -41,18 +121,33 @@ export class ClrTooltipContent extends AbstractPopover {
       throw new Error('clr-tooltip-content should only be used inside of a clr-tooltip');
     }
 
+    this.subscriptions.push(
+      this.smartToggleService.openChange.subscribe(openState => {
+        this.open = openState;
+      })
+    );
+
+    this.popoverId = 'clr-tooltip-' + clrTooltipIds++;
+
     // Defaults
     this.position = 'right';
     this.size = 'sm';
-
-    // Set the default id in case consumer does not supply a custom id.
-    this.updateId(uniqueId);
   }
 
-  private _position: string;
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => {
+      sub.unsubscribe();
+    });
+  }
 
-  get position() {
-    return this._position;
+  public get open() {
+    return this.smartToggleService.open;
+  }
+
+  public set open(open: boolean) {
+    if (!!open !== this.smartToggleService.open) {
+      this.smartToggleService.open = !!open;
+    }
   }
 
   get id(): string {
@@ -70,49 +165,31 @@ export class ClrTooltipContent extends AbstractPopover {
     this.tooltipIdService.updateId(id);
   }
 
+  private _position: string;
   @Input('clrPosition')
   set position(position: string) {
     // Ugh
-    this.renderer.removeClass(this.el.nativeElement, 'tooltip-' + this.position);
     if (position && POSITIONS.indexOf(position) > -1) {
       this._position = position;
     } else {
       this._position = 'right';
     }
-    // Ugh
-    this.renderer.addClass(this.el.nativeElement, 'tooltip-' + this.position);
 
-    // set the popover values based on direction
-    switch (position) {
-      case 'top-right':
-        this.anchorPoint = Point.TOP_CENTER;
-        this.popoverPoint = Point.LEFT_BOTTOM;
-        break;
-      case 'top-left':
-        this.anchorPoint = Point.TOP_CENTER;
-        this.popoverPoint = Point.RIGHT_BOTTOM;
-        break;
-      case 'bottom-right':
-        this.anchorPoint = Point.BOTTOM_CENTER;
-        this.popoverPoint = Point.LEFT_TOP;
-        break;
-      case 'bottom-left':
-        this.anchorPoint = Point.BOTTOM_CENTER;
-        this.popoverPoint = Point.RIGHT_TOP;
-        break;
-      case 'right':
-        this.anchorPoint = Point.RIGHT_CENTER;
-        this.popoverPoint = Point.LEFT_TOP;
-        break;
-      case 'left':
-        this.anchorPoint = Point.LEFT_CENTER;
-        this.popoverPoint = Point.RIGHT_TOP;
-        break;
-      default:
-        this.anchorPoint = Point.RIGHT_CENTER;
-        this.popoverPoint = Point.LEFT_TOP;
-        break;
+    this.smartPosition = SIMPLE_TO_SMART_POSITIONS[position];
+
+    if (this.smartPosition === undefined) {
+      throw new Error('clr-tooltip-content got unknown clrPosition');
     }
+
+    this._position = position;
+  }
+
+  get position() {
+    return this._position;
+  }
+
+  get positionClass() {
+    return 'tooltip-' + this.position;
   }
 
   private _size: string;
@@ -123,14 +200,14 @@ export class ClrTooltipContent extends AbstractPopover {
 
   @Input('clrSize')
   set size(size: string) {
-    // Ugh
-    this.renderer.removeClass(this.el.nativeElement, 'tooltip-' + this.size);
-    if (size && SIZES.indexOf(size) > -1) {
+    if (size && SIZES.includes(size)) {
       this._size = size;
     } else {
       this._size = 'sm';
     }
-    // Ugh
-    this.renderer.addClass(this.el.nativeElement, 'tooltip-' + this.size);
+  }
+
+  get sizeClass() {
+    return 'tooltip-' + this.size;
   }
 }
--- src/clr-angular/popover/tooltip/tooltip.ts
+++ src/clr-angular/popover/tooltip/tooltip.ts
@@ -3,26 +3,38 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
-import { Component, ElementRef } from '@angular/core';
+import { Inject, Component, ElementRef } from '@angular/core';
+import { UNIQUE_ID, UNIQUE_ID_PROVIDER } from '../../utils/id-generator/id-generator.service';
+
+// @TODO maybe won't need it
+import { TooltipIdService } from './providers/tooltip-id.service';
 import { IfOpenService } from '../../utils/conditional/if-open.service';
 import { POPOVER_HOST_ANCHOR } from '../common/popover-host-anchor.token';
-import { UNIQUE_ID_PROVIDER } from '../../utils/id-generator/id-generator.service';
-import { TooltipIdService } from './providers/tooltip-id.service';
+
+// Smart popover
+import { ClrPopoverToggleService } from '../../utils/popover/providers/popover-toggle.service';
+import { ClrPopoverEventsService } from '../../utils/popover/providers/popover-events.service';
+import { ClrPopoverPositionService } from '../../utils/popover/providers/popover-position.service';
 
 @Component({
   selector: 'clr-tooltip',
+  // @TODO can I remove the div ?
   template: `
+      <div clrPopoverAnchor clrPopoverOpenCloseButton>
         <ng-content></ng-content>
-    `,
-  host: {
-    '[class.tooltip]': 'true',
-  },
+      </div>
+  `,
   providers: [
+    // Smart Popover
+    ClrPopoverToggleService,
+    ClrPopoverEventsService,
+    ClrPopoverPositionService,
+    UNIQUE_ID_PROVIDER,
+    // @TODO remove lines below later
     IfOpenService,
     { provide: POPOVER_HOST_ANCHOR, useExisting: ElementRef },
     // TODO: consider centralizing the unique id string on a service that provides ariaAttributes that need it
     // AriaService in layout/tabs/providers might be a good starting point.
-    UNIQUE_ID_PROVIDER,
     TooltipIdService,
   ],
 })
